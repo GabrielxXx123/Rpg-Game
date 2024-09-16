9cf2ed32e792fc05ef8a6d7075ced473b1bbf377
Alexandrescu Gabriel <gabiala27@yahoo.com>
2024-09-16 18:02:35 +0300

    0.1
    -added bullet and goblin

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,8 +1,21 @@
 ﻿#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <math.h>
 using namespace std;
 using namespace sf;
 
+
+Vector2f normalizeVector(Vector2f vector)
+{
+   float m = sqrt(vector.x * vector.x + vector.y * vector.y);
+   Vector2f normalizedVector;
+
+   normalizedVector.x = vector.x / m;
+   normalizedVector.y = vector.y / m;
+
+   return normalizedVector;
+}
 
 int main()
 {//--------------------------------------------------------------------INITIALIZE---------------------------------------------------------------------------------
@@ -10,10 +23,17 @@ int main()
 	ContextSettings settings;
 	settings.antialiasingLevel = 8;
 	
-	RenderWindow window(VideoMode(1200, 800), "Gabriel's Game" , Style::Default, settings);
+	RenderWindow window(VideoMode(1620, 950), "Gabriel's Game" , Style::Default, settings);
+	
+	Texture playerTexture;
+	Sprite playerSprite;
+	Texture enemyTexture;
+	Sprite enemySprite;
+	Texture bulletTexture;
+	vector <Sprite> bullets;
+	Sprite newBullet;
+	float bulletSpeed = 1.1f;
 
-	
-	
 
 	CircleShape circle(50.21f);
 	circle.setPosition(Vector2f(600.00f, 308.21f));
@@ -23,14 +43,33 @@ int main()
 
 	
 	
+
+	
+
+	
+
+	
 	//--------------------------------------------------------------------INITIALIZE---------------------------------------------------------------------------------
 	
 
 
 
 	//--------------------------------------------------------------------LOAD---------------------------------------------------------------------------------
-	Texture playerTexture;
-	Sprite playerSprite;
+	
+	// creating an bullet
+
+	if (bulletTexture.loadFromFile("Assets/Environment/Textures/bullet.png"))
+	{
+		cout << "Bullet image loaded!";
+		newBullet.setTexture(bulletTexture);
+		newBullet.setTextureRect(IntRect(0, 0, 6, 10));
+	}
+	else
+	{
+		cout << "Bullet image failed loading!";
+	}
+	
+	// creating an player
 
 	if (playerTexture.loadFromFile("Assets/Player/Textures/spritesheets.png"))
 	{
@@ -45,10 +84,33 @@ int main()
 	}
 	else
 	{
-		cout << "Player image failed loaded!" << endl;
+		cout << "Player image failed loading!" << endl;
 	}
 	playerSprite.setPosition(Vector2f(600.00f, 308.21f));
 
+
+
+	// creating an enemy
+
+	if (enemyTexture.loadFromFile("Assets/Enemy/Textures/goblin.png"))
+	{
+		cout << "Enemy image loaded!" << endl;
+		enemySprite.setTexture(enemyTexture);
+		enemySprite.scale(2, 2);
+		enemySprite.setTextureRect(IntRect(0, 0, 68, 62));
+	}
+	else
+	{
+		cout << "Enemy image failed loading!" << endl;
+	}
+	enemySprite.setPosition(Vector2f(650.00f, 308.21f));
+
+
+
+
+
+
+	
 	//--------------------------------------------------------------------LOAD---------------------------------------------------------------------------------
 
 
@@ -61,6 +123,15 @@ int main()
 	while (window.isOpen())
 	{
 		//--------------------------------------------------------------------UPDATE---------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
 
 		Event event;
 		while (window.pollEvent(event))
@@ -77,6 +148,10 @@ int main()
 				}
 			}
 		}
+
+		  
+
+
 			// player controller
 
 		
@@ -97,8 +172,29 @@ int main()
 				playerSprite.setPosition(playerSprite.getPosition() + Vector2f(0.5f, 0.0f));
 			}
 
+			// Daca click e apasat creem un bullet in vectorul bullets si ii setam textura si pozitia
 
+			if (Mouse::isButtonPressed(Mouse::Button::Left))
+			{
+				bullets.push_back(newBullet);
+				newBullet.setPosition(playerSprite.getPosition());
+				
+				
 
+				
+
+				
+			}
+			// Calculam directia unde sa se indrepte glontul pentru fiecare glont
+
+			for (size_t i = 0; i < bullets.size(); i++)
+			{
+
+				
+				Vector2f direction = enemySprite.getPosition() - newBullet.getPosition();
+				direction = normalizeVector(direction);
+				bullets[i].setPosition(bullets[i].getPosition() + direction * bulletSpeed);
+			}
 		//--------------------------------------------------------------------UPDATE---------------------------------------------------------------------------------
 
 
@@ -112,8 +208,13 @@ int main()
 		//--------------------------------------------------------------------DRAW---------------------------------------------------------------------------------
 
 		window.clear(Color::Black);
-		//window.draw(circle);
+	    window.draw(enemySprite);
 		window.draw(playerSprite);
+		for (int i = 0; i < bullets.size(); i++)
+		{
+			window.draw(bullets[i]);
+		}
+	
 		window.display();
 		
 
